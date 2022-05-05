03c44261db226cc7329e0c920dc52d1fd7b0369d
杜捧玲 <dupengling@bonc.com.cn>
2022-05-05 16:54:23 +0800

    增加接口参数

---
+++ src/main/java/com/bonc/jibei/entity/ReportParamsMap.java
@@ -0,0 +1,34 @@
+package com.bonc.jibei.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+
+@Data
+@TableName("jb_model_params_map")
+public class ReportParamsMap {
+    @TableId(type = IdType.AUTO)
+    @ApiModelProperty("id")
+    private Integer id;
+
+    @ApiModelProperty("组织ID")
+    private Integer orgId;
+
+    @ApiModelProperty("模板ID")
+    private Integer modelId;
+
+    @ApiModelProperty("接口ID")
+    private Integer interId;
+
+    @ApiModelProperty("参数")
+    private String param;
+
+    @ApiModelProperty("参数名")
+    private String paramName;
+
+    @ApiModelProperty("映射参数")
+    private String mapParam;
+}
---
+++ src/main/java/com/bonc/jibei/mapper/InterParamsMapper.java
@@ -0,0 +1,11 @@
+package com.bonc.jibei.mapper;
+
+import com.bonc.jibei.entity.InterParams;
+
+/**
+ * @Author: dupengling
+ * @DateTime: 2022/5/5 16:46
+ * @Description:  接口参数
+ */
+public interface InterParamsMapper  extends  RootMapper<InterParams>{
+}
