467753db2cc70f7ec3045ce21922ea257534432b
WhoNoName <wjx9428@hotmail.com>
2022-06-21 09:56:13 +0800

    台账设备

--- src/main/java/com/bonc/jibei/mapper/DeviceMapper.java
+++ src/main/java/com/bonc/jibei/mapper/DeviceMapper.java
@@ -2,10 +2,9 @@ package com.bonc.jibei.mapper;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.bonc.jibei.entity.Code;
-import com.bonc.jibei.entity.InterParams;
+import com.bonc.jibei.entity.CodeType;
+import com.bonc.jibei.entity.DeviceModel;
 import com.bonc.jibei.vo.CodeTypeVo;
-import com.bonc.jibei.vo.ModelInterParamMapVo;
-import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -19,4 +18,24 @@ public interface DeviceMapper extends  RootMapper<Code>{
     int selectDeviceListCount();
 
     List<CodeTypeVo>  selectdeviceParam(String pId);
+
+    int  insetCodeType(CodeTypeVo vo);
+
+    int  insetDeviceCompany(String Company);
+    int  updateCodeType(CodeType codeType);
+
+    List<DeviceModel>  selectDeviceModelList(String deviceType, String deviceModel, String deviceCompany );
+
+    /**
+     * 查询设备型号下拉框
+     *
+     * @return 设备型号
+     */
+    List<Code> SelectdropDownType(String dropDownType );
+
+
+//    DeviceModel  SelectDeviceInfoByType(String deviceType);
+
+
+    List<DeviceModel> getDeviceInfo();
 }
