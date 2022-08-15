7981286a70f703bef0d18caa37b028d2220fbe36
wangtao <1154957208@qq.com>
2022-08-15 09:52:53 +0800

    质量模块代码新增

--- src/main/java/com/bonc/jibei/util/JsonUtil.java
+++ src/main/java/com/bonc/jibei/util/JsonUtil.java
@@ -17,7 +17,7 @@ public class JsonUtil {
     /**
      * param,stationId:场站ID；typeId：场站类型ID;modelId:模板ID;starttime:开始时间；endtime：捷顺时间
      **/
-    public static JSONObject createJson(Integer stationId, Integer typeId, Integer modelId, String starttime, String endtime, Integer reportId) {
+    public static JSONObject createJson(Integer stationId, Integer typeId, Integer modelId, String starttime, String endtime, Integer reportId,String resReportName) {
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("stationId", stationId);
         jsonObject.put("typeId", typeId);
@@ -25,6 +25,7 @@ public class JsonUtil {
         jsonObject.put("startTime", starttime);
         jsonObject.put("endTime", endtime);
         jsonObject.put("reportId", reportId);
+        jsonObject.put("resReportName", resReportName);
         return jsonObject;
     }
 }
