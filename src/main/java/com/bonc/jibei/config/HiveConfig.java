9eaaf9a1882ae2de3b3448d54c6c0431a8e90156
wangtao <1154957208@qq.com>
2022-10-19 14:50:59 +0800

    修改Hive配置

--- src/main/java/com/bonc/jibei/config/HiveConfig.java
+++ src/main/java/com/bonc/jibei/config/HiveConfig.java
@@ -14,23 +14,35 @@ public class HiveConfig {
     public List<Map<String, Object>> query(String sql) throws SQLException {
         Connection connection = DriverManager.getConnection("jdbc:hive2://24.43.105.36:10000/src", "hadoop", "hadoop");
         Statement statement = connection.createStatement();
-        System.out.println("++++++++++++++");
+        System.out.println(sql);
         ResultSet rs = statement.executeQuery(sql);
         HashMap<String, Object> hs = new HashMap<>();
         HashMap<String, Object> map2 = new HashMap<>();
         HashMap<String, Object> map1 = new HashMap<>();
         List<Map<String, Object>> maps = new ArrayList<>();
-        while (rs.next()){
-            String value = rs.getString("y1");
-            String y2 = rs.getString("y2");
-            String x1 = rs.getString("x1");
-            hs.put("y1",value);
-            hs.put("y2",y2);
-            hs.put("x1",x1);
-            maps.add(hs);
+        try {
+            ResultSetMetaData metaData = rs.getMetaData();
+            int columnCount = metaData.getColumnCount();
 
+            while (rs.next()) {
+                HashMap<String, Object> rowData = new HashMap<>();
+                for (int i=1;i<=columnCount;i++){
+                    if ("x1".equals(metaData.getColumnName(i))){
+                        rowData.put(metaData.getColumnName(i), rs.getString(i).substring(0,rs.getString(i).length()-2));
+                    }else {
+                        rowData.put(metaData.getColumnName(i),rs.getString(i));
+                    }
+                }
+                String value = rs.getString("y1");
+                String x1 = rs.getString("x1");
+                hs.put("x1", x1);
+                hs.put("y1", value);
+                maps.add(rowData);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
-
+        System.out.println(maps);
         statement.close();
         connection.close();
         return maps;
